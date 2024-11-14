5e2a158290a1d160357d7edb7aa90bd6d84d72cd
nicolas711anaczewski <nicolas.anaczewski@escola.pr.gov.br>
2024-11-14 08:40:53 -0300

    Create quantidadedeUsuarios.js

---
+++ graficos/quantidadedeUsuarios.js
@@ -0,0 +1,16 @@
+async function quantidadeUsuariosPorRede() {
+    const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
+    const res = await fetch(url)
+    const dados = await res.json()
+    const nomeDasRedes = Object.keys(dados)
+    const quantidadeDeUsuarios = Object.values(dados)
+
+    const data = [
+        {
+            x: nomeDasRedes, 
+            y: quantidadeDeUsuarios, 
+            type: 'bar'
+        }
+    ]
+
+    
